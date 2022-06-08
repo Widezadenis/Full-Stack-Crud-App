c5b399c7f2cd3aba9b2c73b4e6f50d8ac8bee049
Wideza Denis <Widezad@gmail.com>
2022-06-08 00:33:51 -0400

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,41 @@
+const update = document.querySelector('#update-button')
+const deletebutton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'put',
+        headers: {'content-Type' : 'application/json'},
+        body: JSON.stringify({
+            name: 'Darth Vader',
+            quote: 'I find your lack of faith disturbing'
+        })
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+    })
+    .then(response => {
+        window.location.reload(true)
+    })
+})
+
+deletebutton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vader'
+        })
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+    })
+    .then(response => {
+        if(response === 'no quote to delete') (
+        messageDiv.textContent = 'no Darth vader quote to delete'
+        )
+        else {
+        window.location.reload(true)
+        }
+    })
+})
